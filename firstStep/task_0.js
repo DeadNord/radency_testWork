43eced91dae2721dc1c2ac899e1018835b59fe59
DeadNord <eosipopo@gmail.com>
2022-04-04 17:18:36 +0300

    v0

---
+++ firstStep/task_0.js
@@ -0,0 +1,13 @@
+// cd firstStep
+// node task_0.js
+
+const chooseOptimalDistance = (t, k, ls) => {
+  // твій код
+  console.log(t, k, ls);
+  return null;
+};
+
+console.log("te");
+
+chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]); //173
+chooseOptimalDistance(163, 3, [50]); // null
