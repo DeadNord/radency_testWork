cf52fc2a900f2f7797daf9574f840856d4c36a4c
DeadNord <eosipopo@gmail.com>
2022-04-12 22:52:54 +0300

    table+hbs

---
+++ secondStep/task_1/js/notesRender.js
@@ -0,0 +1,20 @@
+import data from '../json/data.json';
+import notesList from '../layouts/notes.hbs';
+
+// Ссылки
+const refs = {
+  notesTable: document.querySelector('div.js-table--notes'),
+  //   btn: document.querySelector('[data-action="add-note"]'),
+};
+
+// Слушатели
+
+// refs.galleryList.addEventListener("click", onClickHandler);
+// refs.btn.addEventListener("click", onCloseHandler);
+
+const renderNotes = arr => {
+  const activeNotes = arr.filter(item => item.status === true);
+  refs.notesTable.insertAdjacentHTML('beforeEnd', notesList(activeNotes));
+};
+
+renderNotes(data);
